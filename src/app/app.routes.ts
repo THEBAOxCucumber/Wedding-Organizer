efeb33bf40910178fb324476fe2d22862bfc94a8
THEBAOxCucumber <piriyaporn4849@gmail.com>
2026-03-27 00:32:53 +0700

    Wedding Organizer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OrganizationComponent } from './organization/organization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "about", component: AboutComponent},
+    {path: "organization", component: OrganizationComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  size = 200;
+
+  aboutImageInfo() {
+    return `About Image - Size: ${this.size}x${this.size * 0.8}`;
+  }
+}
